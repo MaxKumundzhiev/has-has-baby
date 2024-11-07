4fa3ee888634732bbaa59a506b09dbc29a3007ad
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2024-11-07 22:30:35 +0500

    class-relationships, 7nd November.

---
+++ patterns/practical-design-patterns-python/classes-relationships/main.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+from enum import Enum
+
+class Gender(Enum):
+    MALE = "male"
+    FEMALE = "female"
+
+
+class Person:
+    def __init__(self, name: str) -> None:
+        self.name = name
+    
+    def __str__(self):
+        return f'{self.name} is alive!!!'
+
+
+# inheretance
+class Man(Person):
+    gender = Gender.MALE  # assosiation
+
+
+# inheretance
+class Woman(Person):
+    gender = Gender.FEMALE  # assosiation
+
+
+class Company:
+    """
+    compamy is composition of employees
+    **company owns employees**
+    """
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.employees = []
+
+    def __str__(self) -> str:
+        return f"company {self.name} with employees: {len(self.employees)}"
+
+    def hire_employee(self, person: Person, position: str) -> None:
+        employee = Employee(person=person, company=self, position=position)
+        self.employees.append(employee)
+        return
+    
+    def liquidate(self) -> None:
+        self.employees = []
+        return
+
+
+class Employee:
+    """
+    employee is aggregation of person and company
+    **employee is composed of person and company**
+    """
+    def __init__(self, person: Person, company: Company, position: str):
+        self.person = person    # aggregation
+        self.company = company  # aggregation
+        self.position = position
+    
+    def __str__(self):
+        return f"{self.person.name} works at {self.company.name} at position {self.position}"
+    
+
+if __name__ == "__main__":
+    jonh = Man("John")
+    mike = Man("Mike")
+    lily = Woman("Lily")
+    company = Company("CocaCola")
+
+    company.hire_employee(jonh, "developer")
+    company.hire_employee(mike, "analyst")
+    company.hire_employee(lily, "qa")
+    company.liquidate() # company is deleted, but people still exists (aggregation property)
---
+++ patterns/practical-design-patterns-python/classes-relationships/notes.md
@@ -0,0 +1,40 @@
+
+# Types of interactions within classes
+1. Inheritance
+2. Assosiation
+3. Aggregation
+4. Composition
+
+## Inheritance
+- Class inherits data and functionalities of another class (successor has all the fields and methods of anchestor) and commonly some additional functionality is added. 
+- Follow principle `B is A`
+- In fact is handy mechansim of DRY concept
+```text
+Example: Car -> Coupe
+```
+
+## Assosiation
+- Classes assosiation is a relationship between classes, which allows one class (object) to call another class (object) and compose some logic from its name.
+- `assosiation` means `dependency`
+- Follows principle `B uses A` or `B interacts with A`
+```text
+Example: student goes to the universtity (uses uni to get knowledge).
+```
+Assosiation has 2 more subtypes: `aggregation` and `composition`
+### Aggregation
+Method of creating a new class (container) B from A.
+- objects are equal
+- objects might exist independently
+- follow principle `B has A`
+- if container will be deleted - another container might still leave
+
+### Composition
+Method of creating a new class (container) B from A.
+- objects are not equal
+- objects might not exist independently
+- follow principle `B owns A`
+- if container will be deleted - another container will be deleted as well
+
+
+## Examle of Aggregation and Composition
+Text editor owns `buffer` (composition). And uses file (aggregation). When closing text editor, buffer is deleted, however file remains.

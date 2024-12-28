bd08949e5a49a99f36dd8395e16606b71b855314
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2024-12-28 14:26:11 +0500

    preorder iterative implementation, 28th December;

---
+++ algorithms/problems/week-0-trees/How-To-Traverse-Trees/dfs/iterative/preorder.py
@@ -0,0 +1,29 @@
+# https://www.youtube.com/watch?v=afTpieEZXck&t=279s
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0):
+        self.left = None
+        self.right = None
+        self.val = val
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]):
+        curr, stack = root, []
+        res = []
+        # while node is not termaination or stack is not empty
+        while curr or stack:
+            # case, when we need to process node
+            if curr:
+                # since, its preorder 
+                # firstly we process node
+                res.append(curr.val)
+                # secondly, we add right child of the node to the stack
+                stack.append(curr.right)
+                # move further to the left
+                curr = curr.left
+            else:
+                # pop the last node from stack
+                curr = stack.pop()
+        return res

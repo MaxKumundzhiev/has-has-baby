9ed4069ce0c9605b7460cfb43d03ddc06c403aa7
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2026-03-28 17:55:49 +0400

    main, iterator;

---
+++ dsa/iter_impl.py
@@ -0,0 +1,41 @@
+"""
+iterators - итераторы - концепт (протокол) который умеет отдавать следующий обьект.
+у него есть два основных метода
+    __iter__
+    __next__
+
+__iter__ - отдает сам обьект (итератор)
+__next__ - отдает следующий элемент или выкидывает ошибку StopIteration - когда элементов не остается
+"""
+
+from typing import TypeVar, Generic, Iterable
+
+T = TypeVar("T")
+
+
+class IteratorImpl(Generic[T]):
+    def __init__(self, iterable: list[T]) -> None:
+        self._container: list[T] = iterable
+        self._index: int = 0
+
+    def __iter__(self) -> "IteratorImpl[T]":
+        return self
+
+    def __next__(self) -> T:
+        if self._index >= len(self._container):
+            raise StopIteration
+        val: T = self._container[self._index]
+        self._index += 1
+        return val
+
+
+if __name__ == "__main__":
+    it = IteratorImpl([1, 2, 3, 4])
+    for v in it:
+        print(v)
+
+    it = IteratorImpl([1, 2, 3, 4])
+    print(next(it))
+    print(next(it))
+    print(next(it))
+    print(next(it))
